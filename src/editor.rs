4c570f0561698032551a7638312ad6155f546536
Matthew Chun <mchun2121@gmail.com>
2023-11-26 19:06:46 -0800

    create new editor file move code, and run it from main

---
+++ src/editor.rs
@@ -0,0 +1,44 @@
+use std::io:: {self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+
+    //Termion gives us access to Key::Char, Key::Ctrl, and Key::Alt
+    //Termion also handles the character instead of printing btyes to chars.
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys(){
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+
+                        if c == 'q' {
+                            break;
+                        }
+                    }
+
+                    Key::Ctrl('q') => break, 
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+
+
+        }
+    }
+}
+
+//Error handle
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
+use editor::Editor;
 
-//Let's map our control-q to the quit operation 
-//Has some issues with vscode
 
-//Error handle
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
 
 fn main() {
-
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-        for key in io::stdin().keys(){
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-
-                    Key::Alt('q') => break, 
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => die(err),
-            }
-
-
-        }
+    let editor = Editor{};
+    editor.run();
 
 }
 
