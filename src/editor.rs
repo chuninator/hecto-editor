23c14f442e2a3c6013a3b3743c52938c1767a94b
Matthew Chun <mchun2121@gmail.com>
2023-11-27 09:53:33 -0800

    access to terminal via file, helper functions, etc

--- src/editor.rs
+++ src/editor.rs
@@ -1,77 +1,65 @@
-use std::io:: {self, stdout, Write};
+use crate::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 pub struct Editor {
-    should_quit: bool, 
+    should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
     pub fn run(&mut self) {
-
-    //Termion gives us access to Key::Char, Key::Ctrl, and Key::Alt
-    //Termion also handles the character instead of printing btyes to chars.
-        let _stdout = stdout().into_raw_mode().unwrap();
+        //Termion gives us access to Key::Char, Key::Ctrl, and Key::Alt
+        //Termion also handles the character instead of printing btyes to chars.
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
             }
             if self.should_quit {
-                break; 
+                break;
             }
             if let Err(error) = self.process_keypress() {
                 die(error);
             }
-
-
         }
     }
 
-
     pub fn default() -> Self {
-        Self{should_quit: false}
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to init terminal"),
+        }
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..self.terminal.size().height {
+            println!("~\r");
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
+        if self.should_quit {
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(0, 0);
+        }
+        Terminal::flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Char('q') => self.should_quit = true,
             _ => (),
         }
         Ok(())
     }
-
-    fn draw_rows(&self) {
-        for _ in 0..24 {
-            println!("~\r");
-        }
-    }
-
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
-        if self.should_quit {
-            println!("Goodbye.\r");
-        } else {
-            self.draw_rows();
-            print!("{}", termion::cursor::Goto(1,1));
-        }
-        io::stdout().flush()
-    }
-
-}
-
-
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
 }
 
 //Error handle
-fn die(e:std::io::Error) {
-    print!("{}", termion::clear::All);
+fn die(e: std::io::Error) {
+    Terminal::clear_screen();
     panic!("{}", e);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod editor;
+mod terminal;
 
-
+pub use terminal::Terminal; 
 use editor::Editor;
 
 fn main() {
