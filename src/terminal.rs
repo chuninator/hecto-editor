f1bca9b01e1ef7da13ce0ccfd25ec729168729be
Matthew Chun <mchun2121@gmail.com>
2023-11-27 09:55:38 -0800

    :quit

--- src/terminal.rs
+++ src/terminal.rs
@@ -39,7 +39,7 @@ impl Terminal {
         io::stdout().flush()
     }
 
-    pub fn read_key() -> Result<(Key), std::io::Error> {
+    pub fn read_key() -> Result<Key, std::io::Error> {
         loop {
             if let Some(key) = io::stdin().lock().keys().next() {
                 return key;
