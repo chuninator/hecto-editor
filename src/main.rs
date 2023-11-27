d6c0caf63ed17c0bdf7b03bd0bf2536a88636372
Matthew Chun <mchun2121@gmail.com>
2023-11-26 18:58:18 -0800

    remove Read from stdlib, and match keys from termion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 //Let's map our control-q to the quit operation 
 //Has some issues with vscode
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
 
 //Error handle
 fn die(e: std::io::Error) {
@@ -17,29 +15,25 @@ fn main() {
 
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
+        for key in io::stdin().keys(){
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    }
 
-        match b {
-            Ok(b) => {
-                let c = b as char; 
-                if c.is_control() {
-                    println!("{:?} \r", b)
-                } else {
-                    println!("{:?} ({})\r", b, c);
-        
-                }
-          
-        
-                if c == 'q' {
-                    break; 
-                }
-
+                    Key::Alt('q') => break, 
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
             }
 
-            Err(err) => die(err),
 
         }
 
-    }
-
 }
+
