435214d2fe9c0ae9bf5e4f188159ea73e012efd4
Matthew Chun <mchun2121@gmail.com>
2023-11-27 09:02:24 -0800

    at least say goodbye when you leave

--- src/editor.rs
+++ src/editor.rs
@@ -44,6 +44,9 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+        if self.should_quit {
+            println!("Goodbye.\r");
+        }
         io::stdout().flush()
     }
 
@@ -60,5 +63,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 
 //Error handle
 fn die(e:std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
