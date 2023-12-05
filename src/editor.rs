20c96756ee6b50cc1ba20669288660ef0d6509b0
Matthew Chun <mchun2121@gmail.com>
2023-12-04 22:30:19 -0800

    adding more highlighting

--- src/editor.rs
+++ src/editor.rs
@@ -410,6 +410,8 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
+                    editor.document.highlight(Some(query));
+
                 },
             )
             .unwrap_or(None);
@@ -417,6 +419,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
+        self.document.highlight(None);
     }
 }
 
